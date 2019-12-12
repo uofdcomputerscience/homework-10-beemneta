ed53d6c161f07bd31735cf215e32252a9ede04b0
Beemnet Alemayehu <balemayehu@Roberts-Mini-2.udallas.edu>
2019-12-11 22:36:09 -0600

    error types

--- ResultHomework.playground/Contents.swift
+++ ResultHomework.playground/Contents.swift
@@ -2,11 +2,38 @@ import Foundation
 
 // Implement an Error type. Make sure it has at least two values.
 
-
 // Implement a function that returns a Result of string or your error type
-
 
 // Call your function in a way that will return an error result, and handle that error.
 
-
 // Call your function in a way that will return a success result, and handle the value.
+
+
+enum codingError: Error {
+    case undeclaredVariable
+    case incorrectSyntax
+    case noReturnValue
+}
+
+func didNotCompile(value: Int) -> Result<String, Error> {
+    if (value >= 15) {
+        return .success("Access Granted")
+    }
+    return .failure(codingError.noReturnValue)
+}
+
+let result = didNotCompile(value: 15)
+
+switch result {
+case .success(let string):
+    print("\(string)")
+case .failure(let error):
+    print(error)
+}
+let res = didNotCompile(value:30)
+switch res {
+  case .failure(let string):
+    print("well: \(string)")
+  case .success(let string):
+    print("\(string)")
+}
